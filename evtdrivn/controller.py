469e714df960654bf3f8eee00e1050a19e82a7aa
zsaim <zzsaim@163.com>
2020-01-21 14:55:52 +0800

    ADD: 控制器实现一些常用方法。

--- evtdrivn/controller.py
+++ evtdrivn/controller.py
@@ -7,12 +7,7 @@ from .signal import (EVT_DRI_SHUTDOWN, EVT_DRI_AFTER, EVT_DRI_SUBMIT,
                      EVT_DRI_BEFORE, EVT_DRI_OTHER)
 from evtdrivn.utils import Listener
 
-try:
-    # py3
-    from queue import Queue
-except ImportError:
-    # py2
-    from Queue import Queue
+from .utils import Queue
 
 from .plugins.base import PluginManager
 from .mapping import MappingManager
@@ -32,7 +27,7 @@ class Controller:
 
         # 为了更好的在各个控制器之间的友好通信，listeners 存放着当前控制器所被监听的事件。
         # 当控制器发生被监听的事件的时候，控制器会将事件（值和上下文会以ForwardingPacket对象）转发给监听者。
-        self._listeners = []
+        self.listeners = []
 
         # 当前控制器线程对象。
         self.__con_thread = None
@@ -49,8 +44,8 @@ class Controller:
         # idle 显示控制器是否处于空闲状态。这里所说的空闲的意思是：
         #   控制器是否处于在处理函数中。
         #   ！！注意的是，当控制器处于取出事件过程中，控制器是处于空闲状态的。！！
-        self.__no_suspend = Event()
-        self.__no_suspend.set()
+        self.__not_suspended = Event()
+        self.__not_suspended.set()
         self.__idle = Event()
 
         # skip事件用于跳过当前事件，若已经处于事件处理过程中的话将无效。
@@ -64,7 +59,23 @@ class Controller:
 
     @property
     def name(self):
+        """ 返回控制器名称。 """
         return self._name
+
+    @property
+    def __global__(self):
+        """ 返回全局上下文环境。"""
+        return self._global
+
+    @property
+    def __runtime__(self):
+        """ 返回运行时上下文环境。"""
+        return self._runtime
+
+    @property
+    def __static__(self):
+        """ 返回静态上下文环境。"""
+        return self._static
 
     def is_idle(self):
         """ 返回控制器是否处于空闲状态。
@@ -78,11 +89,11 @@ class Controller:
 
     def is_suspended(self):
         """ 返回控制器是否被挂起。 """
-        return not self.__no_suspend.is_set()
+        return not self.__not_suspended.is_set()
 
     def suspend(self):
         """ 挂起控制器，等待新的任务进入。 """
-        self.__no_suspend.clear()
+        self.__not_suspended.clear()
         # 挂起插件。
         for plugins in self.plugins:
             for plugin in plugins:
@@ -90,7 +101,7 @@ class Controller:
 
     def resume(self):
         """ 从挂起中恢复。 """
-        self.__no_suspend.set()
+        self.__not_suspended.set()
 
         # 恢复插件。
         for plugins in self.plugins:
@@ -132,16 +143,16 @@ class Controller:
 
     def listen(self, target, allow):
         """ 监听指定控制器的事件。 """
-        target._listeners.append(Listener(self.event_channel, allow))
+        target.listeners.append(Listener(self.event_channel, allow))
 
     def listened_by(self, queue, allow):
         """ 允许被队列Queue监听。"""
-        self._listeners.append(Listener(queue, allow))
+        self.listeners.append(Listener(queue, allow))
 
     def shutdown(self):
         """ 发送关闭控制器的信号。"""
         # 恢复被挂起的线程，让其恢复接受任务处理的状态。
-        self.__no_suspend.set()
+        self.__not_suspended.set()
 
         # 关闭插件。
         for plugins in self.plugins:
@@ -162,9 +173,9 @@ class Controller:
             raise RuntimeError('controller has already been running.')
 
         if suspend:
-            self.__no_suspend.clear()
+            self.__not_suspended.clear()
         else:
-            self.__no_suspend.set()
+            self.__not_suspended.set()
 
         context = dict(context or {})
         self._runtime = context
@@ -189,6 +200,13 @@ class Controller:
         if self.__con_thread and self.__con_thread.is_alive():
             self.__con_thread.join(timeout)
 
+    def wait_for_idle(self, timeout=None):
+        """ 等待控制器进入空闲状态。
+        进入空闲状态并不意味着再下一刻不会进入工作状态。
+        因为本质上空闲状态是当控制器线程处于事件获取的过程中，控制器是处于空闲状态的。
+        """
+        self.__idle.wait(timeout)
+
     def skip(self):
         """ 跳过当前事件的处理。
         通常这用于事件处理之前的hook_before事件。
@@ -199,8 +217,8 @@ class Controller:
         self.__skip = True
 
     def pend(self):
-        """ 等待一个事件处理完毕。"""
-        self.__idle.wait()
+        """ 等待事件处理队列被取完。"""
+        self.event_channel.join()
 
     def clean(self):
         """ 清空消息队列。
@@ -230,8 +248,9 @@ class Controller:
             # 进入空闲状态，即没有任务处理的状态。
             self.__idle.set()
             # 如果线程被挂起，那么进入等待。
-            self.__no_suspend.wait()
+            self.__not_suspended.wait()
             evt, val, event_ctx, hdl_args, hdl_kwargs = self.event_channel.get()
+            self.event_channel.task_done()
             # print('%s, %s, %s, %s, %s, %s, %s' % (current_process().pid, current_thread().ident, evt, val, event_ctx, hdl_args, hdl_kwargs))
             # 当取到任务后清除空闲标志位。
             self.__idle.clear()
@@ -249,7 +268,7 @@ class Controller:
                     hdl_list = [val]
 
             # 以监听事件响应来通知所有监听该事件的监听者。
-            s_push = (i for i in self._listeners if i.check(evt))
+            s_push = (i for i in self.listeners if i.check(evt))
             for i in s_push:
                 # 抄送事件响应信息。
                 i.push(evt, val, event_ctx)
