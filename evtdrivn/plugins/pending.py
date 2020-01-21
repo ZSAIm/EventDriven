d8f51ded56384c2c9d123d0b6f41ce246491fefa
zsaim <zzsaim@163.com>
2020-01-21 14:58:34 +0800

    INFO: 优化插件pending代码结构。

--- evtdrivn/plugins/pending.py
+++ evtdrivn/plugins/pending.py
@@ -12,30 +12,30 @@ class EventPending(BasePlugin):
     def __init__(self):
         # 由于一个控制器的线性执行，所以只需要一个列表来存储pending对象就行了，
         # 当执行完一个事件后，列表的第一个元素就是当前执行的任务。
-        self._pending_events = []
+        self._unfinished_events = []
         # 加锁是为了保证pending返回事件的正确顺序
         self._lock = Lock()
 
     def __patch__(self):
         def dispatch(*args, **kwargs):
             with self._lock:
-                pending_evt = Pending()
-                self._pending_events.append(pending_evt)
+                pending = Pending()
+                self._unfinished_events.append(pending)
 
-                disp(*args, **kwargs)
-                return pending_evt
+                dispatch_super(*args, **kwargs)
+                return pending
 
         def submit(function=None, args=(), kwargs=None, context=None):
             return dispatch(EVT_DRI_SUBMIT, function, context, args, kwargs)
 
-        disp = self._parent.dispatch
+        dispatch_super = self._parent.dispatch
 
         self._parent.dispatch = dispatch
         self._parent.submit = submit
 
     def __return__(self):
         """ 返回操作。"""
-        pending = self._pending_events.pop(0)
+        pending = self._unfinished_events.pop(0)
         pending.set(session['returns'])
 
     def __mapping__(self):
